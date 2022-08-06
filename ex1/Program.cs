f4fbcfc4d15335e4784c67932bea4321d0b090ba
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-06 16:23:37 +0300

    ex1 done

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -2,12 +2,18 @@
 int n1 = int.Parse(Console.ReadLine());
 Console.Write("Please enter number 2: ");
 int n2 = int.Parse(Console.ReadLine());
-int sqr = n2 * n2;
-if (sqr == n1)
+if (n1 == n2)
 {
-    Console.WriteLine($"Number {n1} IS the square of the {n2}");
+    Console.WriteLine($"Number {n1} and number {n2} are equal!");
 }
 else
 {
-   Console.WriteLine($"Number {n1} IS NOT the square of the {n2}");
+    if (n1 > n2)
+    {
+        Console.WriteLine($"Number {n1} is the MAX. Number {n2} - MIN");
+    }
+    else
+    {
+        Console.WriteLine($"Number {n2} is the MAX. Number {n1} - MIN");
+    }
 }
