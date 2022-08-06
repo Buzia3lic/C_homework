930ce5b7c8cd1d6294639b73ada4f93d8245fff0
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-06 17:27:39 +0300

    ex4 done

---
+++ ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Please enter length: ");
+int size = int.Parse(Console.ReadLine());
+int count = 2;
+if (size > 1)
+{
+    Console.Write("All 'even numbers': ");
+    while (count <= (size - 2) )
+    {
+        Console.Write($"{count}, ");
+        count += 2;
+    }
+     Console.Write($"{count}. ");
+}
+else
+{
+    if (size <= 0)
+    {
+        Console.Write("Length must be greater than 0!");   
+    }
+    else
+    {
+        Console.Write("There is no 'even numbers'!");
+    }
+}
+
+
