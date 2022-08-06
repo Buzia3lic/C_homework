3798301471cca6ba9ac11177a5b33b7a57715596
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-06 17:00:46 +0300

    ex3 done

---
+++ ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Please enter number: ");
+int n1 = int.Parse(Console.ReadLine());
+if (n1 == 0)
+{
+    Console.Write($"You entered {n1}. This is a 'neutral number'");
+}
+else
+{
+    if (n1 % 2 == 0)
+    {
+        Console.Write($"The number {n1} IS! 'even number'");
+    }
+    else
+    {
+        Console.Write($"The number {n1} IS NOT! 'even number'");
+    }
+}
