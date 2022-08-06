a5575676e04904c20ef723e2e5be0f0e998f6ee9
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-06 16:44:41 +0300

    ex2 done

---
+++ ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Please enter number 1: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Please enter number 2: ");
+int n2 = int.Parse(Console.ReadLine());
+Console.Write("Please enter number 3: ");
+int n3 = int.Parse(Console.ReadLine());
+int max = n1;
+if (n2 > max)
+{
+    max = n2;
+}
+if (n3 > max)
+{
+    max = n3;
+}
+Console.Write($"Max number is {max}");
