91c8ae9b19edcbb94d6c35d0bf314001f3c16287
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-06 16:16:21 +0300

    start

---
+++ ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Please enter number 1: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Please enter number 2: ");
+int n2 = int.Parse(Console.ReadLine());
+int sqr = n2 * n2;
+if (sqr == n1)
+{
+    Console.WriteLine($"Number {n1} IS the square of the {n2}");
+}
+else
+{
+   Console.WriteLine($"Number {n1} IS NOT the square of the {n2}");
+}
