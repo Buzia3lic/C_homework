f591f5150cb9bd7d71fac4e8df91b71fe5452675
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-13 17:59:36 +0300

    hm2 ex13

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+int a3 = 0;
+
+if (num/100 == 0)
+{
+    Console.WriteLine("There is no the third number!");
+}
+else
+{
+    Console.WriteLine($"The third number is: {(num % 1000)/100}");
+}
+
