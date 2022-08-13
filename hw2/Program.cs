28fa6b6ef530dec427652e6f979e00ce4d1e6676
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-13 18:12:44 +0300

    hm2 ex15

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please enter number of the day of the week: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1 || num > 7)
+{
+    Console.Write("The number is not correct. It must be between 1 and 7!");
+}
+else
+{
+    if( num == 6 || num == 7)
+    {
+        Console.Write("The day is a weekend!");
+    }
+    else
+    {
+        Console.Write("The day is a working day. We need to work((");
+    }
+}
