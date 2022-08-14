6d12823d32b07c7cd8b30dd2b07c9df82e6ecd54
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-14 18:22:51 +0300

    hw3 ex19 done

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+if (num / 10000 == 0)
+{
+    Console.Write("Number must be 5 digits!");
+}
+else
+{
+    if (num / 10000 == num % 10 && (num / 1000) % 10 == (num % 100) / 10)
+    {
+        Console.Write("Yes. The number is a palindrome!");
+    }
+    else
+    {
+        Console.Write("No. The number IS NOT a palindrome!");
+    }
+}
+
