26adbea7e439a4226ceed91cde6f51077d554f6e
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-14 18:29:30 +0300

    hw3 ex21 done

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter X0: ");
+int x0 = int.Parse(Console.ReadLine());
+Console.Write("Enter Y0: ");
+int y0 = int.Parse(Console.ReadLine());
+Console.Write("Enter Z0: ");
+int z0 = int.Parse(Console.ReadLine());
+Console.Write("Enter X1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Enter Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Enter Z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
+if (x0 == x1 && y0 == y1 && z0 == z1) 
+{ 
+    Console.Write("The coordinates of the points are the same!"); 
+}
+else
+{
+    double length = Math.Sqrt(Math.Pow(x0 - x1, 2) + Math.Pow(y0 - y1, 2) + Math.Pow(z0 - z1, 2));
+    Console.Write($"The length is: {length:f2}");
+}
