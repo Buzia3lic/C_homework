f1887f123a005f69b81805921cdc0c84bc871f63
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-14 18:36:35 +0300

    hw3 ex23 done

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"All cubed numbers for numbers 1 to {num}: ");
+int i = 0;
+for (i = 1; i < num; i++)
+{
+    Console.Write($"{i}^3={Math.Pow(i, 3)}, ");
+}
+Console.WriteLine($"{i}^3={Math.Pow(i, 3)}.");
