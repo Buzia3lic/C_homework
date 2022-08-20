e634db1f22727f9f41623bce98dec4c7204381b2
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-20 14:19:08 +0300

    hw4-ex27 done

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int Sum(int a)
+{
+    if (a == 0)
+    {
+        return -1;
+    }
+
+    if (a < 0)
+    {
+        a *= -1;
+    }
+
+    int sum = 0;
+    while (a != 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+
+    return sum;
+}
+
+if (Sum(num) == -1)
+{
+    Console.Write($"Цифр нет");
+}
+else
+{
+    Console.Write($"Сумма цифр в числе {num} равна: {Sum(num)}.");
+}
